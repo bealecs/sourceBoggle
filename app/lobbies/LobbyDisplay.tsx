5a671ebd9f9b5df354386c383692c7ebbafc695f
bealecs <cliftonscottbeale@gmail.com>
2025-01-19 18:53:31 -0600

    style: attempting to perfect the lobbies page

--- app/lobbies/LobbyDisplay.tsx
+++ app/lobbies/LobbyDisplay.tsx
@@ -22,11 +22,11 @@ export const LobbyDisplay = ({ params }: { params: Lobby[] }) => {
   <a href="/" className="absolute top-0 left-0 underline text-blue-500 p-2">Back to home</a>
   
   {/* Header Row */}
-  <div className="grid grid-cols-4 border-2">
-    <p className="border-r-2 p-4">Game status:</p>
-    <p className="border-r-2 p-4">Lobby Code:</p>
-    <p className="border-r-2 p-4">Player count:</p>
-    <p className="p-4">Action:</p>
+  <div className="grid grid-cols-4 items-center">
+    <p className="border-2">Game status:</p>
+    <p className="border-2 text-center">Lobby Code:</p>
+    <p className="border-2 text-center">Player count:</p>
+    <p className="border-2 text-center">Action:</p>
   </div>
   
   {/* Data Rows */}
@@ -45,12 +45,12 @@ export const LobbyDisplay = ({ params }: { params: Lobby[] }) => {
         <li className="p-4 border-r-2">
           <span className="text-blue-500">{lobby.players.length}</span>
         </li>
-        <li className="p-4 text-center">
+        <li className="p-4 text-center border-r-2">
           {lobby.game_active ? (
             <div className="relative">
-            <p className="text-red-500 w-fit mx-auto cursor-pointer" onMouseEnter={() => setActiveGameTooltip(true)} onMouseLeave={() => setTimeout(() => setActiveGameTooltip(false), 300)}>Active game</p>
+            <p className="text-red-500 w-fit mx-auto cursor-pointer" onMouseEnter={() => setActiveGameTooltip(true)} onMouseLeave={() => setTimeout(() => setActiveGameTooltip(false), 300)}>Unavailable</p>
             
-            {activeGameTooltip && <div className="absolute bg-gray-200 text-black border-2 border-yellow-300 rounded-xl p-2 text-sm text-left lg:w-7/12 ">
+            {activeGameTooltip && <div className="absolute bg-gray-200 text-black border-r-2 border-yellow-300 rounded-xl p-2 text-sm text-left lg:w-7/12 ">
               <p>This lobby is joinable, but the game is currently active. You must wait until the game finishes to join.</p>
             </div>}
             </div>
